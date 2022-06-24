fc08a22404fa29bbde03da648c268045f462f38a
mfairborn23 <mfairborn23@users.noreply.github.com>
2022-06-24 15:00:09 -0700

    edit made with giveDistance

--- Cuda/Genetic_Algorithm/sort.cpp
+++ Cuda/Genetic_Algorithm/sort.cpp
@@ -113,7 +113,7 @@ void giveRank(std::vector<Adult> & allAdults, const cudaConstants* cConstants) {
 //gives each adult in the allAdults a distance representing how different it is from other individuals
 void giveDistance(std::vector<Adult> & allAdults, const cudaConstants* cConstants){
     //int that counts the number of the valid adults
-    int validAdults;
+    int validAdults = 0;
     //starting rankSort to make sure nans are at the end of the array.
     std::sort(allAdults.begin(), allAdults.end(), rankSort);
 
@@ -149,14 +149,12 @@ void giveDistance(std::vector<Adult> & allAdults, const cudaConstants* cConstant
         //distance = distance + abs((i+1) - (i-1))
         allAdults[i].distance = allAdults[i].distance + abs((normalPosDiffLeft - normalPosDiffRight));// /(allAdults[validAdults - 1].posDiff - allAdults[0].posDiff));
     }
-
     //Repeat above process for speedDiff
     if(cConstants->missionType == Rendezvous){//only do this for the rendezvous mission since it has 2 objectives
         std::sort(allAdults.begin(), allAdults.begin() + validAdults, LowerSpeedDiff);
-
         //if an individual has a speedDiff in the convergence range, then these individuals will be sorted by their posDiff as well
         int sortUsingPosDiff = 0;
-        while(allAdults[sortUsingPosDiff].speedDiff < cConstants->speed_threshold){
+        while(allAdults[sortUsingPosDiff].speedDiff < cConstants->speed_threshold && sortUsingPosDiff < allAdults.size()){
             sortUsingPosDiff++;
         }
         std::sort(allAdults.begin(), allAdults.begin() + sortUsingPosDiff, LowerPosDiff);

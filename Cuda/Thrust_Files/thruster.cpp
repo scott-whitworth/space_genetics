77f64cadf977849fb5f40d4356e3264d3f50ce45
Sierra <sierrawrocks@gmail.com>
2023-06-21 10:09:47 -0700

    adding new thrusters

--- Cuda/Thrust_Files/thruster.cpp
+++ Cuda/Thrust_Files/thruster.cpp
@@ -13,13 +13,31 @@ thruster<T>::thruster(const cudaConstants* gConfig) {
         type = THRUST_TYPE::NEXT_C;
         P0 = NEXTP0;
     }
+
+    // setting values (defined in header) for when type 1 is called (SPT_140)
+    else if (gConfig->thruster_type == THRUST_TYPE::SPT_140) {
+        type = THRUST_TYPE::SPT_140;
+        P0 = SPTP0;     
+    } 
+
+    // setting values (defined in header) for when type 1 is called (AEPS)
+    else if (gConfig->thruster_type == THRUST_TYPE::AEPS) {
+        type = THRUST_TYPE::AEPS;
+        P0 = AEPSP0;     
+    }                                                 
     coastThreshold = gConfig->coast_threshold;
 }
 
 template <class T> T thruster<T>::calc_eff(const T & Pin) {
     // Data interpolation for thruster type NEXT
     if (type == THRUST_TYPE::NEXT_C) {
-        return  -1.328086e-23*pow(Pin,6) + 6.207694e-19*pow(Pin,5) - 9.991813e-15*pow(Pin,4) +  7.701266e-11*pow(Pin,3) - 3.136031e-07*pow(Pin,2) +  6.805225e-04*Pin;       // Polynomial fit
+        return  -1.328086E-23*pow(Pin,6) + 6.207694E-19*pow(Pin,5) - 9.991813E-15*pow(Pin,4) +  7.701266E-11*pow(Pin,3) - 3.136031E-07*pow(Pin,2) +  6.805225E-04*Pin;       // Polynomial fit
+    }
+    else if (type == THRUST_TYPE::SPT_140) {
+        return  -7.141057E-23*pow(Pin,6) + 1.703637E-18*pow(Pin,5) - 1.775691E-14*pow(Pin,4) + 1.035756E-10*pow(Pin,3) - 3.525689E-07*pow(Pin,2) + 6.628786E-04*Pin;   // Polynomial fit
+    }
+    else if (type == THRUST_TYPE::AEPS) {
+        return   -1.869804E-24*pow(Pin,6) + 1.025302E-19*pow(Pin,5) - 2.221817E-15*pow(Pin,4) + 2.423269E-11*pow(Pin,3) - 1.398300E-07*pow(Pin,2) + 4.131622E-04*Pin;   // Polynomial fit
     }
     else return 0;
 }
@@ -37,6 +55,28 @@ template <class T> T thruster<T>::calc_m_Dot(const T & Pin) {
         }
         else {
             return NEXTm_Dot0;
+        }
+    }
+    else if (type == THRUST_TYPE::SPT_140) {
+        if (Pin < 500){
+            return 0;
+        }
+        else if (Pin < 6000) {
+            return 3.044520E-09*Pin;
+        }
+        else {
+            return SPT140m_Dot0;
+        }
+    }
+    else if (type == THRUST_TYPE::AEPS) {
+        if (Pin < 3120) {
+            return 0;
+        }
+        else if (Pin < 6700) {
+            return 3.248871E-09*Pin;
+        }
+        else  {
+            return AEPSm_Dot0;
         }
     }
     else return 0;
@@ -73,12 +113,12 @@ template <class T> __host__ __device__ T calc_accel(const T & radius, const T & 
     T thrust;
 
     // Power going into the spacecraft as a function of the radius of the spacecraft from the sun (r is non-dimensionalized by dividing by 1 AU).
-    T separation = sqrt( pow(radius, 2) + pow(z, 2) );
-    Pin = thrusterType.P0/pow(separation, 2); 
+    T craftSunDist = sqrt( pow(radius, 2) + pow(z, 2) );
+    Pin = thrusterType.P0/pow(craftSunDist, 2); 
 
     //If the spacecraft is closer to the sun than the earth, the power in can not be greater than the power experimentally measured on earth.
     //This creates a "sphere" around the sun to ensure the power does not exceed the tested limit.
-    if (separation <= 1) {
+    if (craftSunDist <= 1) {
         Pin = thrusterType.P0; // It is devided by 1 astronomical unit to normalize it P0/(1 AU)
     }
 
